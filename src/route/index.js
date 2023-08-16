7f41515ec73b617c8eafe795fcce4aeff3b7feb2
unknown <deyakvasya906@gmail.com>
2023-08-16 14:30:20 +0300

    final_fixed_version

--- src/route/index.js
+++ src/route/index.js
@@ -555,14 +555,14 @@ router.get('/template-4', function (req, res) {
       },
       {
         heading: "Featured title",
-        info: "This is a wider card with supporting text below as a natural lead-in to additional content",
+        info: "Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.",
         linkText: "Call to action",
         linkUrl: "https://example.com/",
         photo:  "https://picsum.photos/64/64"
       },
       {
         heading: "Featured title",
-        info: "This is a wider card with supporting text below as a natural lead-in to additional content",
+        info: "Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.",
         linkText: "Call to action",
         linkUrl: "https://example.com/",
         photo:  "https://picsum.photos/64/64"
